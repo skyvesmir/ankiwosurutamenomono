dfca5135d50ce15a2931de61d45c60a958b1ebf2
Sky Vesmir <skyfaminfinity@gmail.com>
2026-06-29 09:43:31 +0900

    Update firebase-auth.js

--- vocaforge/public/static/firebase-auth.js
+++ vocaforge/public/static/firebase-auth.js
@@ -30,7 +30,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getFirestore(app,'vocaforge');
 const provider = new GoogleAuthProvider();
 
 // ログイン状態をローカルに永続化（再訪時も維持）
